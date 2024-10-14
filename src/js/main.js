cf0c9bc422b1ec617e59d61c9ef622d4605e322c
minjeongss <dotoriido@gmail.com>
2024-10-14 15:36:24 +0900

    feat: webpack polyfill 환경 설정

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import "@babel/polyfill";
 import { pi, power, Foo } from "./lib";
 
 console.log(pi);
@@ -6,3 +7,14 @@ console.log(power(pi, pi));
 const f = new Foo();
 console.log(f.foo());
 console.log(f.bar());
+
+// polyfill 필요한 부분
+console.log(
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(1), 100);
+  })
+);
+
+console.log(Object.assign({}, { x: 1 }, { y: 2 }));
+
+console.log(Array.from([1, 2, 3], (v) => v + v));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 module.exports = {
-  entry: "./src/js/main.js",
+  entry: ["@babel/polyfill", "./src/js/main.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "js/bundle.js",
